e5fabea06d2672ad3877a508ccdabc01d0fa626b
matia <Matias_gramkow@hotmail.com>
2020-03-29 12:58:25 +0200

    Initial commit

---
+++ src/main/java/com/mandatory/database/MandatoryDatabaseOrmApplication.java
@@ -0,0 +1,13 @@
+package com.mandatory.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandatoryDatabaseOrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MandatoryDatabaseOrmApplication.class, args);
+    }
+
+}
